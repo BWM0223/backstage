d10b87e22e6b597ada811594498e48abb06c7465
Johan Persson <johanopersson@gmail.com>
2026-06-05 13:00:36 +0200

    fix(catalog): clear stale entity during navigation
    Signed-off-by: Johan Persson <johanopersson@gmail.com>

--- plugins/catalog/src/alpha/pages.tsx
+++ plugins/catalog/src/alpha/pages.tsx
@@ -279,7 +279,10 @@ export const catalogEntityPage = PageBlueprint.makeWithOverrides({
 
         const Component = () => {
           const entityFromUrl = useEntityFromUrl();
-          const { entity } = entityFromUrl;
+          const entity = entityFromUrl.loading
+            ? undefined
+            : entityFromUrl.entity;
+          const entityProviderProps = { ...entityFromUrl, entity };
           const filteredMenuItems = entity
             ? menuItems.filter(i => i.filter(entity)).map(i => i.element)
             : [];
@@ -314,7 +317,7 @@ export const catalogEntityPage = PageBlueprint.makeWithOverrides({
             );
 
           return (
-            <AsyncEntityProvider {...entityFromUrl}>
+            <AsyncEntityProvider {...entityProviderProps}>
               {layout}
             </AsyncEntityProvider>
           );
